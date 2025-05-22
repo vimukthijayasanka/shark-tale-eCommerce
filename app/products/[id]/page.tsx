3aa69875cbe001f0fd991d5bed1b69d20be2fc67
Vimukthi Jayasanka <jayasanka1997@gmail.com>
2025-05-22 15:02:49 +0530

    Add 404 not found page

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,14 +1,21 @@
 import { products } from "@/app/product-data";
+import NotFound from "next/dist/client/components/not-found-error";
+import NotFoundPage from "@/app/not-found";
 
 export default function ProductDetailPage({params} : {params : {id: string}}) {
 
     const product = products.find( p => p.id === params.id );
+
+    if (!product) {
+        return <NotFoundPage/>
+    }
+
     return (
         <>
-            <h1>{product!.name}</h1>
-            <p>{product!.price}</p>
+            <h1>{product.name}</h1>
+            <p>{product.price}</p>
             <h3>Description</h3>
-            <p>{product!.description}</p>
+            <p>{product.description}</p>
         </>
 
     )
