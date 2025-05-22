e5a4e31736dd0ea8e31e2700b0171eac4fce88bd
Vimukthi Jayasanka <jayasanka1997@gmail.com>
2025-05-23 01:22:51 +0530

    Impl get all productAPI

--- app/api/db.ts
+++ app/api/db.ts
@@ -1,10 +1,14 @@
 import dotenv from "dotenv";
 dotenv.config();
-import { MongoClient, ServerApiVersion } from 'mongodb';
+import { MongoClient, Db, ServerApiVersion } from 'mongodb';
+
+let cachedClient: MongoClient | null = null;
+let cachedDb: Db | null = null;
 
 export async function connectToDB() {
-    console.log("MONGODB_USER:", process.env.MONGODB_USER);
-    console.log("MONGODB_PASSWORD:", process.env.MONGODB_PASSWORD);
+    if (cachedClient && cachedDb) {
+        return { client: cachedClient, db: cachedDb };
+    }
 
     const uri = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@sharktale0.a9mrneu.mongodb.net/?retryWrites=true&w=majority&appName=SharkTale0`;
     console.log(uri);
@@ -16,24 +20,10 @@ export async function connectToDB() {
             deprecationErrors: true,
         }
     });
+    await client.connect();
 
-    try {
-        await client.connect();
-        const db = client.db('sharktale-db');
-        const productionCollection = db.collection('carts');
+    cachedClient = client;
+    cachedDb = client.db('sharktale-db');
 
-        const result = await productionCollection.insertMany([
-            {
-                userId: '1', cartIds: ['p001', 'p002']
-            },
-            {
-                userId: '2', cartIds: ['p003', 'p004']
-            }
-        ]);
-        console.log("inserted document =>", result.insertedCount);
-    } finally {
-        await client.close();
-    }
+    return { client, db: client.db()}
 }
-
-connectToDB().catch(console.error);
--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -1,6 +1,9 @@
-import {products} from "@/app/product-data";
+import { connectToDB } from "@/app/api/db";
 
 export async function GET() {
+    const {db} = await connectToDB();
+    const products = await db.collection("products").find({}).toArray();
+
     return new Response(JSON.stringify(products),
         {status: 200,
         headers: {
