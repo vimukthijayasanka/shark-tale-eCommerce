2a0b395eda9eb2305ff8cf464d5ab38e349a24ef
Vimukthi Jayasanka <jayasanka1997@gmail.com>
2025-05-23 10:02:12 +0530

    Impl cart route

--- app/api/products/[id]/route.ts
+++ app/api/products/[id]/route.ts
@@ -1,14 +1,18 @@
 import { NextRequest } from "next/server";
-import {products} from "@/app/product-data";
+import { connectToDB } from "@/app/api/db";
 
 type Params = {
     id: string;
 }
 
 export async function GET(request: NextRequest, {params}: {params: Params}) {
+    const {db} = await connectToDB();
+
     const productId = params.id;
 
-    const product = products.find(p => p.id === productId);
+    const product = await db.collection("products").findOne({
+        id: productId
+    })
 
     if (!product) {
         return new Response('Product not found!', {
--- app/api/users/[id]/cart/route.ts
+++ app/api/users/[id]/cart/route.ts
@@ -1,13 +1,13 @@
 import { NextRequest } from "next/server";
-import {products} from "@/app/product-data";
+import {connectToDB} from "@/app/api/db";
 
-type ShoppingCart = Record<string, string[]>;
-
-const carts: ShoppingCart = {
-    '1': ['p001', 'p002'],
-    '2': ['p003', 'p004'],
-    '3': ['p004'],
-}
+// type ShoppingCart = Record<string, string[]>;
+//
+// const carts: ShoppingCart = {
+//     '1': ['p001', 'p002'],
+//     '2': ['p003', 'p004'],
+//     '3': ['p004'],
+// }
 
 type Params = {
     id: string;
@@ -15,32 +15,44 @@ type Params = {
 
 
 export async function GET(request:NextRequest, {params}: {params: Params}) {
-    const userId = params.id;
-    const productIds = carts[userId];
-    const cartProducts = productIds.map(id=> products.find(p=>p.id===id));
+   const {db} = await connectToDB();
 
-    if (productIds === undefined) {
+    const userId = params.id;
+    const userCart = await db.collection('carts').findOne({userId});
+
+    if (!userCart) {
+
         return new Response(JSON.stringify([]),{
             status: 200,
             headers: {"Content-Type": "application/json",}
         });
     }
 
+    const cartIds = userCart.cartIds;
+    const cartProducts = await db.collection('product')
+        .find({id: {$in: cartIds}}).toArray();
+
     return new Response(JSON.stringify(cartProducts), {
             status: 200,
             headers: {"Content-Type": "application/json"}
         })
-    }
+}
 
 type CartBody = {productId: string};
 
 export async function POST(request:NextRequest, {params}: {params: Params}) {
+    const {db} = await connectToDB();
+
     const userId = params.id;
     const body: CartBody = await request.json();
     const productId = body.productId;
 
-    carts[userId] = carts[userId] ? carts[userId].concat(productId) : [productId];
-    const cartProducts =carts[userId].map(id=> products.find(p=>p.id===id));
+    const updatedCart = await db.collection('carts')
+        .findOneAndUpdate({userId},
+            {$push: {cartIds: productId}},
+            { upsert: true, returnDocument: 'after' });
+
+    const cartProducts = await db.collection('products').find({id: { $in: updatedCart.cartIds}}).toArray();
 
     return new Response(JSON.stringify([cartProducts]), {
         status: 201,
@@ -49,13 +61,25 @@ export async function POST(request:NextRequest, {params}: {params: Params}) {
 }
 
 export async function DELETE(request:NextRequest, {params}: {params: Params}) {
+    const {db} = await connectToDB();
+
     const userId = params.id;
     const body = await request.json();
     const productId = body.productId;
 
-    carts[userId] = carts[userId] ? carts[userId].filter(pid => pid !== productId) : [];
+    const updatedCart = await db.collection('carts')
+        .findOneAndUpdate({userId},{$pull: {cartIds: productId}},{returnDocument: 'after'});
 
-    return new Response(JSON.stringify(carts[userId]), {
+    if (!updatedCart) {
+        return new Response(JSON.stringify([]), {
+            status: 202,
+            headers: {"Content-Type": "application/json"}
+        })
+    }
+
+    const cartProducts = await db.collection('products').find({id: { $in: updatedCart.cartIds}}).toArray();
+
+    return new Response(JSON.stringify(cartProducts), {
         status: 202,
         headers: {"Content-Type": "application/json",}
     })
